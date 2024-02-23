ceefd9db6f71c3a76e84dbd528a0f0bade5632c5
Viktor Aretinsky <victor.aretinsky@gmail.com>
2024-02-23 17:29:06 +0300

    Initialize empty project

---
+++ src/main/java/org/aretinsky/lib/apierrorfactory/ApiErrorFactoryApplication.java
@@ -0,0 +1,13 @@
+package org.aretinsky.lib.apierrorfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiErrorFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiErrorFactoryApplication.class, args);
+    }
+
+}
